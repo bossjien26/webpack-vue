b39918e16130e7d061b473d29d947f2ce6ce1668
jan <bossjien26@gmail.com>
2022-01-15 00:46:58 +0800

    feat:add footer

--- src/common/index.js
+++ src/common/index.js
@@ -7,7 +7,8 @@ Vue.use(VModel)
 Vue.use(BootstrapVue);
 Vue.use(ElementUI);
 
-import '../view/router/router.js';
+import '../view/router/router';
+import '../view/layout/footer';
 import VModel from 'vue-js-modal'
 require("babel-core/register");
 require("babel-polyfill");
