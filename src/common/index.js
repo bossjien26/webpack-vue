47ad384d13ef4c31b5c198d7a35dde1dce93fa45
jan <bossjien26@gmail.com>
2022-01-14 23:02:57 +0800

    feat:add common file

---
+++ src/common/index.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+Vue.use(VModel)
+Vue.use(BootstrapVue);
+Vue.use(ElementUI);
+
+import '../view/router/router.js';
+import VModel from 'vue-js-modal'
+require("babel-core/register");
+require("babel-polyfill");
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
