63176564a523c7f13e5956e69fb40af6ed468e17
jan <bossjien26@gmail.com>
2022-01-15 00:47:46 +0800

    feat:rename parameter

--- src/view/router/router.js
+++ src/view/router/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
-import Headers from "../router/router.vue";
+import Router from "../router/router.vue";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 
@@ -8,5 +8,5 @@ Vue.use(BootstrapVue);
 Vue.use(ElementUI);
 
 export default new Vue({
-    render: h => h(Headers)
+    render: h => h(Router)
 }).$mount("#header");
