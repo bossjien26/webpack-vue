d78a7ad26019abe3151ad9d27379aad5fab17ca1
jan <bossjien26@gmail.com>
2022-01-15 00:46:21 +0800

    feat:remove no use package

--- src/view/product/index.js
+++ src/view/product/index.js
@@ -2,10 +2,6 @@ import Vue from "vue";
 import '../../common/index';
 import List from './list.vue';
 
-
-Vue.use(VModel)
-Vue.use(BootstrapVue);
-
 new Vue({
     // el: "#app",
     // render: h => h(List),
