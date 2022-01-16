a8df015381277288de3bcf53cf37479552f1a0eb
jan <bossjien26@gmail.com>
2022-01-16 23:51:26 +0800

    feat:add user page

---
+++ src/view/user/user.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">
+      roles:
+      <!-- <span v-for="role in roles" :key="role">{{ role }}</span> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Dashboard",
+  computed: {
+    ...mapGetters(["name", "roles"]),
+  },
+  created() {
+    this.getUserInfo();
+  },
+  methods: {
+    async getUserInfo() {
+      var response = await this.$store.dispatch("user/getInfo");
+      console.log(response);
+    },
+  },
+};
+</script>
