d1b9a9a49125d3871189626e568b2e069043a588
jan <bossjien26@gmail.com>
2022-01-20 00:11:50 +0800

    feat:add svg and cart menu

--- src/view/layout/header.vue
+++ src/view/layout/header.vue
@@ -40,9 +40,22 @@
         v-if="!IsLogin"
         >login</el-menu-item
       >
-
-      <el-submenu index="4" style="float: right" v-if="IsLogin">
-        <template slot="title"><i class="el-icon-menu"></i></template>
+      <el-submenu index="4" style="float: right">
+        <template slot="title">
+          <!-- <i class="el-icon-menu"></i> -->
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            class="bi bi-person-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+            />
+          </svg>
+        </template>
         <el-menu-item index="4-1" onclick="location.href='/user'"
           ><i class="el-icon-setting"></i> Settings</el-menu-item
         >
@@ -50,6 +63,24 @@
           ><i class="el-icon-circle-close"></i> logout</el-menu-item
         >
       </el-submenu>
+
+      <el-menu-item
+        style="float: right"
+        index="5"
+        onclick="location.href='/cart'"
+        v-if="IsLogin"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-cart2"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"
+          /></svg
+      ></el-menu-item>
     </el-menu>
   </div>
 </template>
