3d33b425083a8f02693529fa04c8b048408c0d58
jan <bossjien26@gmail.com>
2022-02-20 15:41:06 +0800

    feat:add save user detail

--- src/view/user/components/detail.vue
+++ src/view/user/components/detail.vue
@@ -1,14 +1,38 @@
 <template>
-  <div></div>
+  <el-form
+    :model="userDetail"
+    :rules="cityRules"
+    ref="userDetail"
+    label-width="100px"
+    class="demo-userDetail"
+  >
+    <el-form-item label="City" prop="city">
+      <el-input v-model="userDetail.city"></el-input>
+    </el-form-item>
+    <el-form-item label="Country" prop="country">
+      <el-input v-model="userDetail.country"></el-input>
+    </el-form-item>
+    <el-form-item label="Street" prop="street">
+      <el-input v-model="userDetail.street"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click.native="save">save</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
-import { getInfo } from "../../../api/userDetail";
+import { getInfo, created, updated } from "../../../api/userDetail";
 
 export default {
   data() {
     return {
-      userDetail: {},
+      userDetail: { id: 0 },
+      cityRules: {
+        city: [{ required: true, message: "", trigger: "blur" }],
+        country: [{ required: true, message: "", trigger: "blur" }],
+        street: [{ required: true, message: "", trigger: "blur" }],
+      },
     };
   },
   async created() {
@@ -20,8 +44,26 @@ export default {
         const { data } = await getInfo();
         this.userDetail = data;
       } catch (error) {
-        console.log(error);
+        this.$message.error("ERROR");
       }
+    },
+    async created() {
+      try {
+        await created(this.userDetail);
+      } catch (error) {
+        this.$message.error("ERROR");
+      }
+    },
+    async updated() {
+      try {
+        await updated(this.userDetail);
+      } catch (error) {
+        this.$message.error("ERROR");
+      }
+    },
+    async save() {
+      this.userDetail =
+        this.userDetail.id === 0 ? await this.created() : await this.updated();
     },
   },
 };
